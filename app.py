fafc8f089c26f83967a6ceb7be1f3244809f7c89
stephen <stephenmacgregor9@gmail.com>
2020-04-27 17:22:03 +0100

    bootstrap cdn/added api/index temp

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template, request, flash
+from newsapi import NewsApiClient
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html", title="News Feed")
+    # Init api
+    newsapi = NewsApiClient(api_key='b855866438b64269ba90a58a73b28673')
+    
+    top_headlines = newsapi.get_top_headlines(sources='bbc-news')
+
+    articles = top_headlines['articles']
+    desc = []
+    aut = []
+    news = []
+    link = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        aut.append(myarticles['author'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        link.append(myarticles['url'])
+
+    mylist = zip(news, aut, desc, img, link)
+
+    return render_template("index.html", context=mylist, title="News Feed")
 
 
 
